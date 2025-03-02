2eacef08244379f80498a3e5626a81fa9612d1e3
aashnajoshi <aashnahetal@gmail.com>
2025-03-02 18:39:07 +0530

    .

--- Backend/summit/settings.py
+++ Backend/summit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JS, etc)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / "frontend" / "build" / "static",]
 
 # Media files (Images, Videos, etc)
 MEDIA_URL = '/media/'
@@ -78,6 +78,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ["templates"],
+        # 'DIRS': [BASE_DIR / "frontend" / "build"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
