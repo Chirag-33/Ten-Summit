aab7328db8bf3e820c046cc5c090abc5396daa10
girish1802 <142975821+girish1802@users.noreply.github.com>
2025-03-03 00:28:08 +0530

    responsive

--- Frontend/src/Component/News/News.jsx
+++ Frontend/src/Component/News/News.jsx
@@ -3,7 +3,6 @@ import img from "../../assets/images/newspage-illu-1-1.webp";
 import "./news.css";
 import { newsData } from "../../assets/data/news_data.js";
 
-
 export default function News() {
   const [active, setActive] = useState("in-the-news");
   return (
@@ -27,7 +26,9 @@ export default function News() {
           onClick={() => {
             setActive("press-releases");
           }}
-          className={`tab-button ${active === "press-releases" ? "activetab" : ""}`}
+          className={`tab-button ${
+            active === "press-releases" ? "activetab" : ""
+          }`}
         >
           Press Releases
         </button>
@@ -45,20 +46,48 @@ export default function News() {
           </div>
         ) : (
           <div className="news-container">
-           {
-            newsData.map(({id,img,desc,date})=>{
-              return(
+            {newsData.map(({ id, img, desc, date }) => {
+              return (
                 <div key={id} className="card">
-                  <img src={img} alt="newsimage" style={{height:"50px", width:"300px"}} />
+                  <img
+                    src={img}
+                    alt="newsimage"
+                    style={{ height: "50px", width: "300px" }}
+                  />
                   <h3 className="p1">{desc}</h3>
                   <p className="p2">{date}</p>
                 </div>
-              )
-            })
-           }
+              );
+            })}
           </div>
         )}
       </div>
+     
+      <div className="contact-conatianer">
+        <div className="contact-card">
+          <img
+            src="https://www.scylladb.com/wp-content/uploads/ScyllaDB-Enterprise-Mascot-2.png"
+            alt="conatct-img"
+            style={{ width: "100px", height: "100px" }}
+          />
+          <h2>Contact Us</h2>
+          <p>Drop us a line to get in touch or start a live chat.</p>
+          <a href="#">Let's talk</a>
+        </div>
+       
+        <div className="contact-card">
+          <img
+            src="https://www.scylladb.com/wp-content/uploads/Group-2.png"
+            alt="conatct-img"
+            style={{ width: "200px", height: "100px" }}
+          />
+          <h2>We're Hiring!</h2>
+          <p>Ready to become a Sea Monster?</p>
+          <a href="#">View our open positions</a>
+        </div>
+       
+      </div>
+      
     </>
   );
 }
