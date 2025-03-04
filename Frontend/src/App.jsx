738030bba5ed96956fbf5f0cf0c357e83fac39d7
Saurav <sauravjyotikalita56@gmail.com>
2025-03-04 14:31:55 +0530

    Added the Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,10 +2,11 @@
 import TrustCenter from "./pages/TrustCenter";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import News from "./Component/News/News";
-
+import NavbarX from './pages/NavbarX'
 function App() {
   return (
     <>
+    <NavbarX/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<TrustCenter />} />
