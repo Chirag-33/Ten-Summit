57082ad307472d067ccb11120b422361f88dc618
Saurav <sauravjyotikalita56@gmail.com>
2025-03-07 17:26:43 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,23 @@
-import "./App.css";
-import React from "react";
 import TrustCenter from "./pages/TrustCenter";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import News from "./Component/News/News";
-import NavbarX from './pages/'
+import News from "./pages/News";
+import NavbarX from './pages/NavbarX';
+import Career from "./pages/Career";
+import CookiePolicy from './pages/CookiePolicy/CookiePolicy';
+import Register from './pages/Register';
+
 
 function App() {
   return (
     <>
+      <NavbarX />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<TrustCenter />} />
           <Route path="/news" element={<News />} />
+          <Route path="/careers" element={<Career />} />
+          <Route path="/cookie-policy" element={<CookiePolicy />} />
+          <Route path="/register" element={<Register />} /> 
         </Routes>
       </BrowserRouter>
     </>
