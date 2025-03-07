8f6de94c2bf132789c0abaa9a854e3e3ea9a87d1
Saurav <sauravjyotikalita56@gmail.com>
2025-03-07 18:06:34 +0530

    Create NavbarX.jsx

---
+++ Frontend/src/pages/NavbarX.jsx
@@ -0,0 +1,31 @@
+import  { useState } from "react";
+import "./Navbar.css";
+import Register from "./Register";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a href="/.Monster">Monster SCALE Summit</a>
+      </div>
+      <div className={`navbar-links ${isOpen ? "open" : ""}`}>
+        <a href="#speakers">Speakers</a>
+        <a href="#about">About</a>
+        <a href="#agenda">Agenda</a>
+        <button onClick={() => setShowModal(true)}>Register</button>
+          
+      </div>
+      {showModal && <Register onClose={() => setShowModal(false)} />}
+      <div className="menu-icon" onClick={toggleMenu}>
+        ☰
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
