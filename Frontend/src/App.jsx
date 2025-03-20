9d28d9b02354ce1ed24159a9d1b16d58bb8a0dec
yogita0914 <yogitazee1@gmail.com>
2025-03-20 14:28:42 +0530

    modified routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,9 @@ import Register from './pages/Register';
 import Team from "./pages/Team";
 import Home from "./pages/Home";
 import Navbar from "./pages/Navbar";
+import TermsAndServices from "./pages/TermsAndServices";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import ContactUs from "./pages/ContactUs";
 
 
 function App() {
@@ -21,6 +24,9 @@ function App() {
           <Route path="/cookie-policy" element={<CookiePolicy />} />
           <Route path="/register" element={<Register />} /> 
           <Route path="/team" element={<Team />} /> 
+          <Route path="/terms" element={<TermsAndServices />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </BrowserRouter>
     </>
