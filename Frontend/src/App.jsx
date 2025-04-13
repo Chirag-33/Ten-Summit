1a0e20e9f9297f7606c5d345e28243584a89c050
Saurav <sauravjyotikalita56@gmail.com>
2025-04-14 01:28:35 +0530

    Reverse Change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,40 +1,37 @@
-// App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
 import TrustCenter from "./pages/TrustCenter";
 import News from "./pages/News";
-import Career from "./pages/Career";
 import CookiePolicy from "./pages/CookiePolicy";
 import Register from "./pages/Register";
-import Agenda from "./pages/Agenda";
-import AgendaList from "./pages/AgendaList";
 import Team from "./pages/Team";
+import Home from "./pages/Home";
+import Navbar from "./pages/Navbar";
+import Career from "./pages/Career";
 import TermsAndServices from "./pages/TermsAndServices";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import ContactUs from "./pages/ContactUs";
-
+import Agenda from './pages/Agenda'
+import Footer from "./pages/Footer";
+import AgendaList from "./pages/AgendaList";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        {/* All routes now use Layout */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/trust" element={<TrustCenter />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/careers" element={<Career />} />
-          <Route path="/cookie-policy" element={<CookiePolicy />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/agenda" element={<Agenda />} />
-          <Route path="/agenda-list" element={<AgendaList />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/terms" element={<TermsAndServices />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/contact" element={<ContactUs />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/trust" element={<TrustCenter />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/careers" element={<Career />} />
+        <Route path="/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/terms" element={<TermsAndServices />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/AgendaList" element={<AgendaList />} />
       </Routes>
+      {/* <Footer/> */}
     </BrowserRouter>
   );
 }
--- Frontend/src/pages/Layout.jsx
+++ Frontend/src/pages/Layout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Home from './Home'
 const Layout = () => {
   return (
     <div>
       <Navbar />
+      <Home/>
       <Outlet />
       <Footer />
     </div>
