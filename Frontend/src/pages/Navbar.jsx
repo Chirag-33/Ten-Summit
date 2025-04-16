0fa411d6ed3455dbd415b52fdb6b956a31d6dd15
Saurav <sauravjyotikalita56@gmail.com>
2025-04-16 21:55:01 +0530

    Changes in component in Navbar elements routing

--- Frontend/src/pages/Navbar.jsx
+++ Frontend/src/pages/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars, FaTimes } from "react-icons/fa"; // Import icons
 import { NavLink } from "react-router-dom"; // Import NavLink
 import "./Navbar.css"; // Import CSS
 import Agenda from "./Agenda";
+import ContactUs from "./ContactUs";
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -15,7 +16,7 @@ const Navbar = () => {
       <ul className={menuOpen ? "nav-links active" : "nav-links"}>
         <li><NavLink to="/team" activeClassName="active">Speakers</NavLink></li>
         <li><NavLink to="/agenda" activeClassName="active">Agenda</NavLink></li>
-        <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+        <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
         <li><NavLink to="/register" className="register-btn">Register</NavLink></li>
       </ul>
     </nav>
